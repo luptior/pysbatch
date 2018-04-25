bfb73d6799c5fec2bb22ae020d85e74a4953537a
Gan Xu <ganx@cs.unc.edu>
2018-04-25 17:04:05 -0400

    edit url to GitHub

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pysbatch',
       version='0.1',
       description='Submit slurm job in python',
-      url='https://gist.github.com/luptior/db944ec628e9dde1c49a10e6e70a91be',
+      url='https://github.com/luptior/pysbatch',
       author='Gan Xu',
       author_email='gan_xu@outlook.com',
       license='MIT',
