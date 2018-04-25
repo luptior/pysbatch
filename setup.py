087957ea027024e289fd5e5ba4d91c6218725a0c
Gan Xu <ganx@cs.unc.edu>
2018-04-25 16:54:41 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysbatch',
+      version='0.1',
+      description='Submit slurm job in python',
+      url='https://gist.github.com/luptior/db944ec628e9dde1c49a10e6e70a91be',
+      author='Gan Xu',
+      author_email='gan_xu@outlook.com',
+      license='MIT',
+      packages=['pysbatch'],
+      zip_safe=False)
