1d6de6ecc9ed80110dac9406158659374808b036
Gan Xu <luptior@gmail.com>
2018-04-26 17:20:18 -0400

    add setting object to save set options every time

--- README.md
+++ README.md
@@ -19,6 +19,6 @@ sbatch(wrap="python hello.py") # simplest
 jobid=sbatch(wrap="python hello.py").split(" ")[-1] # dependency example
 sbatch(job_name="py_job", mem=16, dep="--dependency:afterok:{}".format(jobid), days=3, log="submit.out", wrap="python hello.py") # more options
 
-sbatch(job_name="py_job", add_option="--cpus-per-task=1 --nodes=3", wrap="python hello.py") # add more options
+sbatch(job_name="py_job", add_option="--cpus-per-task=1 --nodes=1", wrap="python hello.py") # add more options
 
 ```
--- pysbatch/pysbatch.py
+++ pysbatch/pysbatch.py
@@ -2,6 +2,60 @@ import subprocess as sps
 import sys, os, time
 sys.path.append(os.getcwd())
 
+
+class batch_setting:
+    ntasks=''
+    cpus_per_task=''
+    N=''
+    job_name=''
+    dep=''
+    mem=''
+    days=''
+    log=''
+    options=''
+
+    def __init__(self):
+        self.ntasks='1'
+        self.cpus_per_task='1'
+        self.N='1'
+        self.job_name="py_job"
+        self.dep=""
+        self.mem="8"
+        self.days='3'
+        self.log="submit.out"
+        self.options=""
+
+    def edit_default(self, edit):
+        try:
+            for x in edit.split(" "):
+                var, value = x.split("=")
+                var = '_'.join(filter(None, var.split("-")))
+                print(var)
+                _='value'
+                exec("self.{} = {}".format(var, _))
+        except:
+             sys.exit("Edit string is in wrong format/include options other than default")
+
+
+    def add_options(self, edit):
+        self.options=edit
+
+
+    def sbatch(self, wrap):
+        sub=['sbatch',
+             '--ntasks={}'.format(self.ntasks),
+             '--cpus-per-task={}'.format(self.cpus_per_task), '-N', self.N,
+             '--job-name={}'.format(self.job_name),
+             '--mem={}'.format(self.mem+"000"),
+             '--time={}'.format(self.days+"-0"),
+             self.dep, self.options,
+             '--out={}'.format(self.log),
+             '--wrap="{}"'.format(wrap.strip())]
+        # print(" ".join(sub))
+        process = sps.Popen(" ".join(sub), shell=True, stdout=sps.PIPE)
+        stdout = process.communicate()[0].decode("utf-8")
+        return(stdout)
+
 def sbatch(job_name="py_job", mem='8', dep="", days='3', log="submit.out",wrap="python hello.py", add_option=""):
     sub=['sbatch',
          '--ntasks=1',
@@ -10,8 +64,8 @@ def sbatch(job_name="py_job", mem='8', dep="", days='3', log="submit.out",wrap="
          '--mem={}'.format(mem+"000"),
          '--time={}'.format(days+"-0"),
          dep, add_option,
-         '--out={}'.format(log)]
-    sub.append('--wrap="{}"'.format(wrap.strip()))
+         '--out={}'.format(log),
+         '--wrap="{}"'.format(wrap.strip())]
     # print(" ".join(sub))
     process = sps.Popen(" ".join(sub), shell=True, stdout=sps.PIPE)
     stdout = process.communicate()[0].decode("utf-8")
@@ -22,7 +76,7 @@ def run_cmd(cmd):
     # cmd pass in as a list of strings, i.e. cd .. should be ['cd', '..']
     # return screen print as a string
     process=sps.run(cmd, stdout=sps.PIPE)
-    return process.stdout.decode("utf-8").strip("\n")
+    return process.stdout.decode("utf-8")
 
 def limit_jobs(limit=20000):
     l_jobs=run_cmd(['squeue', '-u', '$USER']).split("\n")
