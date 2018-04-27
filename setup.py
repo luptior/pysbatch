cebdb5770aab64f4b687685a0e8744b800f0cc5e
Aspartame Xu <luptior@gmail.com>
2018-04-27 14:15:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,21 @@ setup(name='pysbatch',
       author_email='luptior@gmail.com',
       license='MIT',
       packages=['pysbatch'],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+    # How mature is this project?
+    'Development Status :: 2 - Pre-Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Bio-Informatics'
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+],
+     )
