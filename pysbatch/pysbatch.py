b8b78c9c8738ffb8f712355059a9f9af2af75882
Gan Xu <luptior@gmail.com>
2018-05-02 15:12:38 -0400

    update main

--- pysbatch/pysbatch.py
+++ pysbatch/pysbatch.py
@@ -1,69 +1,8 @@
 import subprocess as sps
-<<<<<<< HEAD
-import sys, os, time
-sys.path.append(os.getcwd())
-
-
-class batch_setting:
-    ntasks=''
-    cpus_per_task=''
-    N=''
-    job_name=''
-    dep=''
-    mem=''
-    days=''
-    log=''
-    options=''
-
-    def __init__(self):
-        self.ntasks='1'
-        self.cpus_per_task='1'
-        self.N='1'
-        self.job_name="py_job"
-        self.dep=""
-        self.mem="8"
-        self.days='3'
-        self.log="submit.out"
-        self.options=""
-
-    def edit_default(self, edit):
-        try:
-            for x in edit.split(" "):
-                var, value = x.split("=")
-                var = '_'.join(filter(None, var.split("-")))
-                print(var)
-                _='value'
-                exec("self.{} = {}".format(var, _))
-        except:
-             sys.exit("Edit string is in wrong format/include options other than default")
-
-
-    def add_options(self, edit):
-        self.options=edit
-
-
-    def sbatch(self, wrap):
-        sub=['sbatch',
-             '--ntasks={}'.format(self.ntasks),
-             '--cpus-per-task={}'.format(self.cpus_per_task), '-N', self.N,
-             '--job-name={}'.format(self.job_name),
-             '--mem={}'.format(self.mem+"000"),
-             '--time={}'.format(self.days+"-0"),
-             self.dep, self.options,
-             '--out={}'.format(self.log),
-             '--wrap="{}"'.format(wrap.strip())]
-        # print(" ".join(sub))
-        process = sps.Popen(" ".join(sub), shell=True, stdout=sps.PIPE)
-        stdout = process.communicate()[0].decode("utf-8")
-        return(stdout)
-
-def sbatch(job_name="py_job", mem='8', dep="", days='3', log="submit.out",wrap="python hello.py", add_option=""):
-=======
 import sys, os
 
 
 def sbatch(job_name="py_job", mem='8', dep="", time='3-0', log="submit.out",wrap="python hello.py", add_option=""):
->>>>>>> master
     sub=['sbatch',
          '--ntasks=1',
          '--cpus-per-task=1', '-N', '1',
@@ -71,8 +10,8 @@ def sbatch(job_name="py_job", mem='8', dep="", time='3-0', log="submit.out",wrap
          '--mem={}'.format(mem+"000"),
          '--time={}'.format(time),
          dep, add_option,
-         '--out={}'.format(log),
-         '--wrap="{}"'.format(wrap.strip())]
+         '--out={}'.format(log)]
+    sub.append('--wrap="{}"'.format(wrap.strip()))
     # print(" ".join(sub))
     process = sps.Popen(" ".join(sub), shell=True, stdout=sps.PIPE)
     stdout = process.communicate()[0].decode("utf-8")
@@ -86,6 +25,7 @@ def run_cmd(cmd):
     process=sps.run(cmd, stdout=sps.PIPE)
     return process.stdout.decode("utf-8")
 
+
 def limit_jobs(limit=20000):
     l_jobs=run_cmd(['squeue', '-u', '$USER']).split("\n")
     # limit the total number of jobs in slurm job queue
@@ -93,12 +33,6 @@ def limit_jobs(limit=20000):
         time.sleep(300)
     return
 
-def limit_jobs(limit=20000, time=t):
-    l_jobs=run_cmd(['squeue', '-u', '$USER']).split("\n")
-    # limit the total number of jobs in slurm job queue
-    if int(l_jobs) >= int(limit):
-        time.sleep(int(t))
-    return
 
 if __name__ == '__main__':
     sbatch(wrap="python hello.py")
